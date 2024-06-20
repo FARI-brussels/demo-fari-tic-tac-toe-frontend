2af4bae55c4f2bee3c00ad889147644e81f5a904
m0satron <18460264+m0satron@users.noreply.github.com>
2024-06-20 11:11:13 +0200

    feat: add AnimationContainer

---
+++ src/components/AnimationContainer.vue
@@ -0,0 +1,47 @@
+<template>
+  <div ref="lottieContainer" class="lottie-container"></div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import lottie from 'lottie-web'
+
+const props = defineProps<{
+  animationData: Object
+  loop?: boolean,
+  delay?: number,
+}>()
+
+const lottieContainer = ref<HTMLElement | null>(null)
+
+function init({ container, animationData }: { container: HTMLElement; animationData: Object }) {
+  lottie.loadAnimation({
+    container,
+    renderer: 'canvas',
+    loop: props.loop,
+    autoplay: true,
+    animationData
+  })
+}
+
+onMounted(() => {
+  const delay = props.delay ?? 200;
+
+  if (lottieContainer.value && props.animationData) {
+    setTimeout(() => {
+      init({
+        container: lottieContainer.value,
+        animationData: props.animationData
+      })
+    }, delay)
+  }
+})
+</script>
+
+<style scoped lang="scss">
+.lottie-container {
+  width: 276px;
+  height: 247px;
+}
+</style>
+
