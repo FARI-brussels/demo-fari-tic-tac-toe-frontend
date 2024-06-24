f8b04beac891a9cd272c42e7080a9c43bff5a232
m0satron <msebdani@gmail.com>
2024-06-24 09:33:28 +0200

    fix: backend

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import type { Request, Response } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+
+
 import type { 
   DrawGridRequest, 
   DrawGridResponse, 
@@ -12,11 +15,50 @@ import type {
 
 const app = express();
 const PORT = 3000;
+dotenv.config();
+
+const strapiURL = process.env.API_URL
+const EXTERNAL_API_BASE_URL = 'http://127.0.0.1';
+
+interface Attributes {
+  locale: string;
+  logo: string
+  research_head: string
+  research_lead: string
+  explanation_short: {
+    en: string
+    nl: string
+    'fr-FR': string
+  }
+}
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const EXTERNAL_API_BASE_URL = 'http://127.0.0.1'; 
+
+app.get('/api/data', async (req: Request, res: Response) => {
+  try {
+    const response = await fetch(`${strapiURL}/api/demos/52?populate=*`);
+    const {data} = await response.json();
+    const { localizations, explanation_short, research_head, research_lead } = data.attributes
+    const locales = localizations.data.map(({ attributes }: { attributes: Attributes }) => ({ locale: attributes.locale, explanation_short: attributes.explanation_short }))
+
+    const airLogo = await fetch(`${strapiURL}/uploads/air_a5e1c08136.svg`);
+    const logo = await airLogo.text()
+
+    const content = [
+      { logo },
+      { research_head, research_lead },
+      { locale: 'en', explanation_short }, 
+      ...locales
+    ]
+    res.send(content);
+
+  } catch (error) {
+    console.error({error})
+    res.status(500).send('Error fetching data from Strapi');
+  }
+});
 
 
 app.post('/draw_grid', async (req: Request, res: Response) => {
